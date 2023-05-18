b1664e894b9f6681628f9f4ab5f656f6414500e6
kydrid <git@kydrid.lol>
2023-05-18 18:44:42 +0200

    idek

--- routes/api/accounts.py
+++ routes/api/accounts.py
@@ -1,9 +1,10 @@
-import logging
+import passlib
+from fastapi import Request
 
 from utils.classes import APIRouter
 
 router = APIRouter(logger_name=__name__, logger_level=0) # logger level will later be changed when including
 
-@router.get("/")
-async def index():
-    return "Hello, World!"
+@router.post("/register")
+async def index(request: Request):
+    ...
